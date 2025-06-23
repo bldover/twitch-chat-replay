82b2a11bc158c6dbe7c30c42ec971c79e076f362
bldover <dover.bradley@gmail.com>
2025-06-23 01:35:56 -0400

    increase default chat height

--- src/utils/settings.ts
+++ src/utils/settings.ts
@@ -41,7 +41,7 @@ const DEFAULT_SETTINGS: AppSettings = {
     chatPosition: 'right',
     chatDelay: 0,
     chatWidth: 350,
-    chatHeight: 550,
+    chatHeight: 700,
     theme: 'ttv',
     badges: {
         showUserStates: true,
